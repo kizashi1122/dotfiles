c4f0f926f744c751b918213e627ce2ef32168215
Kizashi Nagata <kizashi1122@gmail.com>
2014-04-24 02:55:13 +0000

    Use ruby-mode with Rakefile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -415,6 +415,7 @@
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 
 ;; ruby-mode-hook用の関数を定義
 (defun ruby-mode-hooks ()
