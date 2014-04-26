b0721876eb98e5287b799527869a0e53b873b0f4
Kizashi Nagata <kizashi1122@gmail.com>
2014-04-26 10:12:07 +0000

    Use ruby-mode when opening *.cap (capistrano)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -416,6 +416,7 @@
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.cap$" . ruby-mode))
 
 ;; ruby-mode-hook用の関数を定義
 (defun ruby-mode-hooks ()
