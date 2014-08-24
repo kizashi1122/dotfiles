2a1f7cd6c4b868edfe655f974f368f050e70ede1
Kizashi <kizashi1122@gmail.com>
2014-08-24 17:14:20 +0900

    add descbinds-anything with elpa

---
+++ .emacs.d/elpa/descbinds-anything-1.5/descbinds-anything-autoloads.el
@@ -0,0 +1,18 @@
+;;; descbinds-anything-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("descbinds-anything-pkg.el" "descbinds-anything.el")
+;;;;;;  (21496 39869 50364 788000))
+
+;;;***
+
+(provide 'descbinds-anything-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; descbinds-anything-autoloads.el ends here
