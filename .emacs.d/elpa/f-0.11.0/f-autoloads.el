2b454d1665515d873e02cc8b7b810641a63eb379
Kizashi <kizashi1122@gmail.com>
2014-08-24 17:26:13 +0900

    update elpa relevant files

---
+++ .emacs.d/elpa/f-0.11.0/f-autoloads.el
@@ -0,0 +1,18 @@
+;;; f-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("f-pkg.el" "f.el") (21462 2713 616917
+;;;;;;  822000))
+
+;;;***
+
+(provide 'f-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; f-autoloads.el ends here
