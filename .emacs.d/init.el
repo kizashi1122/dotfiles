d531ea14a6b5b9ed0b2452bfc82c6ede11b6fe9d
Kizashi Nagata <kizashi1122@gmail.com>
2014-12-16 15:45:40 +0900

    web-mode 追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -301,7 +301,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; P158 nxml-modeをHTML編集のデフォルトモードに
 ;; HTML編集のデフォルトモードをnxml-modeにする
-(add-to-list 'auto-mode-alist '("\\.[sx]?html?\\(\\.[a-zA-Z_]+\\)?\\'" . nxml-mode))
+;;(add-to-list 'auto-mode-alist '("\\.[sx]?html?\\(\\.[a-zA-Z_]+\\)?\\'" . nxml-mode))
 ;; ▼要拡張機能インストール▼
 ;;; P159 HTML5をnxml-modeで編集する
 ;; HTML5
@@ -321,6 +321,27 @@
 ;; 属性値のインデント幅を設定する。初期値は4
 (setq nxml-attribute-indent 0)
 
+;; web-mode
+(require 'web-mode)
+;; 拡張子の設定
+(add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+;; インデント関係
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-html-offset   4)
+  (setq web-mode-css-offset    4)
+  (setq web-mode-script-offset 4)
+  (setq web-mode-php-offset    4)
+  (setq web-mode-java-offset   4)
+  (setq web-mode-asp-offset    4)
+  (setq indent-tabs-mode t)
+  (setq tab-width 4))
+(add-hook 'web-mode-hook 'web-mode-hook)
 
 ;; ▼要拡張機能インストール▼
 ;;; P161 cssm-modeの基本設定
